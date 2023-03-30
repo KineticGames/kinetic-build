9f445b9dfec608191b9fd75be776f7806224e802
Kasper de Vries <kasper.j.s.de.vries@gmail.com>
2023-03-30 17:54:08 +0200

    Move out project definition & Comment out code to be refactored
    In the future I want to improve the modularity of the project definition so it can become
    its own package.

---
+++ src/project_definition.h
@@ -0,0 +1,45 @@
+#ifndef KINETIC_BUILD_PROJECT_DEFINITION_H
+#define KINETIC_BUILD_PROJECT_DEFINITION_H
+
+// lib
+#include <kinetic_notation.h>
+
+// std
+#include <stdbool.h>
+#include <stddef.h>
+
+#define NAME_KEY "name"
+#define VERSION_KEY "version"
+
+#define LIBRARY_KEY "lib"
+#define LIBRARY_SHARED_KEY "shared"
+
+#define DEPENDENCIES_KEY "dependencies"
+#define DEPENDENCY_NAME_KEY "name"
+#define DEPENDENCY_VERSION_KEY "version"
+#define DEPENDENCY_URL_KEY "url"
+
+typedef struct {
+  char *project_dir;
+  char *name;
+  kn_version version;
+  bool is_lib;
+  struct {
+    bool shared;
+  } lib;
+  size_t dependency_count;
+  struct dependency {
+    char *name;
+    kn_version version;
+    char *url;
+    char *path;
+  } *dependencies;
+} kinetic_project;
+
+kn_definition *kinetic_project_create_definition();
+
+bool kinetic_project_from_dir(char *dir_path, kinetic_project *project);
+
+void kinetic_project_clear(kinetic_project *project);
+
+#endif // KINETIC_BUILD_PROJECT_DEFINITION_H
